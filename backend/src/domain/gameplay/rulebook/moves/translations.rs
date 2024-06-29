589d7b525c1976490d1ff51ec1d387b688d7db1a
edwilson <edward.e.j.wilson@gmail.com>
2024-06-29 09:22:18 +0100

    Add support for factoring out a scalar from chess vectors

--- backend/src/domain/gameplay/rulebook/moves/translations.rs
+++ backend/src/domain/gameplay/rulebook/moves/translations.rs
@@ -36,7 +36,7 @@ impl ops::Mul<i8> for ChessVector {
 #[derive(Debug, PartialEq)]
 struct Translation {
     vector: ChessVector,
-    scale_factor: u8,
+    scalar: u8,
 }
 
 pub fn is_move_allowed_for_piece(
@@ -52,8 +52,12 @@ impl Translation {
         to_square: &chess_set::Square,
         piece_colour: &chess_set::Colour,
     ) -> Self {
-        let x = to_square.get_file().value() - from_square.get_file().value();
-        let y = to_square.get_rank().value() - from_square.get_rank().value();
+        let x_unscaled = to_square.get_file().value() - from_square.get_file().value();
+        let y_unscaled = to_square.get_rank().value() - from_square.get_rank().value();
+
+        let scalar = greatest_common_divisor(x_unscaled, y_unscaled);
+        let x = x_unscaled / scalar;
+        let y = y_unscaled / scalar;
 
         // Vectors for black and white are relative to different origins.
         // This is because they use the same references for each square,
@@ -66,7 +70,7 @@ impl Translation {
 
         Self {
             vector: vector,
-            scale_factor: 1,
+            scalar: scalar as u8,
         }
     }
 }
@@ -82,7 +86,7 @@ fn greatest_common_divisor(a: i8, b: i8) -> i8 {
     let lower = cmp::min(a_abs, b_abs);
 
     if lower == 0 {
-        return higher
+        return higher;
     };
 
     let difference = higher - lower;
@@ -154,19 +158,155 @@ mod tests {
             let white_translation = Translation::new(&from_square, &to_square, &Colour::White);
 
             assert_eq!(white_translation.vector, expected_vector);
-            assert_eq!(white_translation.scale_factor, 1);
+            assert_eq!(white_translation.scalar, 1);
 
             let black_translation = Translation::new(&from_square, &to_square, &Colour::Black);
 
             assert_eq!(black_translation.vector, expected_vector * -1);
-            assert_eq!(black_translation.scale_factor, 1);
+            assert_eq!(black_translation.scalar, 1);
         }
 
-        // TODO
-        fn multiple_square_moves_in_a_straight_line() {}
+        #[rstest]
+        #[case::forwards(
+            Square::new(Rank::ONE, File::A),
+            Square::new(Rank::THREE, File::A),
+            ChessVector::new(0, 1),
+            2
+        )]
+        #[case::forwards_and_right(
+            Square::new(Rank::THREE, File::C),
+            Square::new(Rank::SIX, File::F),
+            ChessVector::new(1, 1),
+            3
+        )]
+        #[case::right(
+            Square::new(Rank::SEVEN, File::D),
+            Square::new(Rank::SEVEN, File::H),
+            ChessVector::new(1, 0),
+            4
+        )]
+        #[case::backwards_and_right(
+            Square::new(Rank::SIX, File::A),
+            Square::new(Rank::ONE, File::F),
+            ChessVector::new(1, -1),
+            5,
+        )]
+        #[case::backwards(
+            Square::new(Rank::SEVEN, File::G),
+            Square::new(Rank::TWO, File::G),
+            ChessVector::new(0, -1),
+            5,
+        )]
+        #[case::backwards_and_left(
+            Square::new(Rank::EIGHT, File::G),
+            Square::new(Rank::TWO, File::A),
+            ChessVector::new(-1, -1),
+            6,
+        )]
+        #[case::left(
+            Square::new(Rank::THREE, File::H),
+            Square::new(Rank::THREE, File::A),
+            ChessVector::new(-1, 0),
+            7,
+        )]
+        #[case::forward_and_left(
+            Square::new(Rank::FOUR, File::F),
+            Square::new(Rank::EIGHT, File::B),
+            ChessVector::new(-1, 1),
+            4,
+        )]
+        fn multiple_square_moves_in_a_straight_line(
+            #[case] from_square: Square,
+            #[case] to_square: Square,
+            #[case] expected_vector: ChessVector,
+            #[case] expected_scalar: u8,
+        ) {
+            let white_translation = Translation::new(&from_square, &to_square, &Colour::White);
 
-        // TODO
-        fn multiple_square_moves_in_a_wonky_line() {}
+            assert_eq!(white_translation.vector, expected_vector);
+            assert_eq!(white_translation.scalar, expected_scalar);
+
+            let black_translation = Translation::new(&from_square, &to_square, &Colour::Black);
+
+            assert_eq!(black_translation.vector, expected_vector * -1);
+            assert_eq!(black_translation.scalar, expected_scalar);
+        }
+
+        #[rstest]
+        #[case::forwards_two_right_one(
+            Square::new(Rank::ONE, File::A),
+            Square::new(Rank::THREE, File::B),
+            ChessVector::new(1, 2)
+        )]
+        #[case::forwards_one_right_two(
+            Square::new(Rank::THREE, File::C),
+            Square::new(Rank::FOUR, File::E),
+            ChessVector::new(2, 1)
+        )]
+        #[case::backwards_one_right_two(
+            Square::new(Rank::THREE, File::C),
+            Square::new(Rank::TWO, File::E),
+            ChessVector::new(2, -1),
+        )]
+        #[case::backwards_two_right_one(
+            Square::new(Rank::SIX, File::A),
+            Square::new(Rank::FOUR, File::B),
+            ChessVector::new(1, -2),
+        )]
+        #[case::backwards_two_left_one(
+            Square::new(Rank::SEVEN, File::G),
+            Square::new(Rank::FIVE, File::F),
+            ChessVector::new(-1, -2),
+        )]
+        #[case::backwards_one_left_two(
+            Square::new(Rank::EIGHT, File::G),
+            Square::new(Rank::SEVEN, File::E),
+            ChessVector::new(-2, -1),
+        )]
+        #[case::forwards_one_left_two(
+            Square::new(Rank::THREE, File::H),
+            Square::new(Rank::FOUR, File::F),
+            ChessVector::new(-2, 1),
+        )]
+        #[case::forwards_two_left_one(
+            Square::new(Rank::FOUR, File::F),
+            Square::new(Rank::SIX, File::E),
+            ChessVector::new(-1, 2),
+        )]
+        fn multiple_square_moves_in_a_wonky_line_scalar_factor_one(
+            #[case] from_square: Square,
+            #[case] to_square: Square,
+            #[case] expected_vector: ChessVector,
+        ) {
+            let white_translation = Translation::new(&from_square, &to_square, &Colour::White);
+
+            assert_eq!(white_translation.vector, expected_vector);
+            assert_eq!(white_translation.scalar, 1);
+
+            let black_translation = Translation::new(&from_square, &to_square, &Colour::Black);
+
+            assert_eq!(black_translation.vector, expected_vector * -1);
+            assert_eq!(black_translation.scalar, 1);
+        }
+
+        #[test]
+        fn multiple_square_moves_in_a_wonky_line_with_scalar_factor() {
+            let from_square = Square::new(Rank::THREE, File::E);
+            let to_square = Square::new(Rank::SEVEN, File::C);
+
+            let expected_vector = ChessVector::new(-1, 2);
+            let expected_scalar_factor = 2;
+
+            let white_translation = Translation::new(&from_square, &to_square, &Colour::White);
+
+            assert_eq!(white_translation.vector, expected_vector);
+            assert_eq!(white_translation.scalar, expected_scalar_factor);
+
+            let black_translation = Translation::new(&from_square, &to_square, &Colour::Black);
+
+            assert_eq!(black_translation.vector, expected_vector * -1);
+            assert_eq!(black_translation.scalar, expected_scalar_factor);
+        }
     }
 
     #[cfg(test)]
