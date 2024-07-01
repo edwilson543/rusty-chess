9407265596d1988e0586374d73f6c35fbf86c678
edwilson <edward.e.j.wilson@gmail.com>
2024-07-01 15:27:31 +0100

    Rename `Command` to `Move`

--- backend/src/domain/gameplay/game.rs
+++ backend/src/domain/gameplay/game.rs
@@ -1,6 +1,31 @@
-use super::commands::{Command, CommandHandlingError};
 use crate::domain::gameplay::chess_set;
 use crate::domain::gameplay::rulebook;
+
+pub enum Move {
+    OrdinaryMove {
+        player: chess_set::Colour,
+        from_square: chess_set::Square,
+        to_square: chess_set::Square,
+    },
+}
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum MoveError {
+    #[error("The game has already ended.")]
+    GameHasAlreadyEnded,
+
+    #[error("Move is out of turn - it's currently {0}'s turn.")]
+    PlayIsOutOfTurn(chess_set::Colour),
+
+    #[error("{0} player attempted to move opponent's piece.")]
+    CannotMoveOpponentPiece(chess_set::Colour),
+
+    #[error("{0}")]
+    MoveValidationError(rulebook::MoveValidationError),
+
+    #[error("{0}")]
+    ChessboardActionError(chess_set::ChessboardActionError),
+}
 
 #[derive(Debug, PartialEq)]
 enum GameStatus {
@@ -13,7 +38,7 @@ enum GameStatus {
 pub struct Game {
     chessboard: chess_set::Chessboard,
     status: GameStatus,
-    command_history: Vec<Command>,
+    move_history: Vec<Move>,
 }
 
 // Public interface.
@@ -24,26 +49,23 @@ impl Game {
 
         Self {
             chessboard: chessboard,
-            command_history: vec![],
+            move_history: vec![],
             status: GameStatus::ToPlay(chess_set::Colour::White),
         }
     }
 
-    pub fn handle_command(
-        &mut self,
-        command: Command,
-    ) -> Result<&GameStatus, CommandHandlingError> {
-        if let Err(handling_error) = match command {
-            Command::MakeMove {
+    pub fn make_move(&mut self, chess_move: Move) -> Result<&GameStatus, MoveError> {
+        if let Err(handling_error) = match chess_move {
+            Move::OrdinaryMove {
                 player,
                 from_square,
                 to_square,
-            } => self.make_move(&player, &from_square, &to_square),
+            } => self.make_ordinary_move(&player, &from_square, &to_square),
         } {
             return Err(handling_error);
         }
 
-        self.command_history.push(command);
+        self.move_history.push(chess_move);
         self.progress_game_status();
         Ok(&self.status)
     }
@@ -63,43 +85,41 @@ impl Game {
         };
     }
 
-    fn make_move(
+    fn make_ordinary_move(
         &mut self,
         player: &chess_set::Colour,
         from_square: &chess_set::Square,
         to_square: &chess_set::Square,
-    ) -> Result<(), CommandHandlingError> {
+    ) -> Result<(), MoveError> {
         // Check the move isn't out of turn.
         let GameStatus::ToPlay(to_play_colour) = self.status else {
-            return Err(CommandHandlingError::GameHasAlreadyEnded);
+            return Err(MoveError::GameHasAlreadyEnded);
         };
         if !(player == &to_play_colour) {
-            return Err(CommandHandlingError::PlayIsOutOfTurn(
-                to_play_colour.clone(),
-            ));
+            return Err(MoveError::PlayIsOutOfTurn(to_play_colour.clone()));
         }
 
         // Check the player is moving a piece of their own colour.
         let Some(piece) = self.get_piece_at_square(&from_square) else {
-            return Err(CommandHandlingError::ChessboardActionError(
+            return Err(MoveError::ChessboardActionError(
                 chess_set::ChessboardActionError::SquareIsEmpty(from_square.clone()),
             ));
         };
         if !(piece.get_colour() == &to_play_colour) {
-            return Err(CommandHandlingError::CannotMoveOpponentPiece(*player));
+            return Err(MoveError::CannotMoveOpponentPiece(*player));
         };
 
         // Check the move against the rulebook.
         if let Err(error) =
             rulebook::validate_move(&self.chessboard, &piece, &from_square, &to_square)
         {
-            return Err(CommandHandlingError::MoveValidationError(error));
+            return Err(MoveError::MoveValidationError(error));
         };
 
         // Finally, move the piece.
         match self.chessboard.move_piece(&from_square, &to_square) {
             Ok(()) => Ok(()),
-            Err(error) => Err(CommandHandlingError::ChessboardActionError(error)),
+            Err(error) => Err(MoveError::ChessboardActionError(error)),
         }
     }
 }
@@ -108,7 +128,7 @@ impl Game {
 mod tests {
 
     #[cfg(test)]
-    mod handle_command_tests {
+    mod make_move_tests {
         use super::super::*;
         use crate::domain::gameplay::chess_set::{Colour, File, Rank};
 
@@ -118,13 +138,13 @@ mod tests {
 
             let from_square = chess_set::Square::new(Rank::Two, File::E);
             let to_square = chess_set::Square::new(Rank::Four, File::E);
-            let opening_move = Command::MakeMove {
+            let opening_move = Move::OrdinaryMove {
                 player: Colour::White,
                 from_square,
                 to_square,
             };
 
-            let result = game.handle_command(opening_move);
+            let result = game.make_move(opening_move);
 
             assert_eq!(result, Ok(&GameStatus::ToPlay(Colour::Black)));
             assert_eq!(game.get_piece_at_square(&from_square), None);
@@ -137,15 +157,15 @@ mod tests {
 
             let from_square = chess_set::Square::new(Rank::Seven, File::C);
             let to_square = chess_set::Square::new(Rank::Six, File::C);
-            let opening_move = Command::MakeMove {
+            let opening_move = Move::OrdinaryMove {
                 player: Colour::Black,
                 from_square,
                 to_square,
             };
 
-            let result = game.handle_command(opening_move);
+            let result = game.make_move(opening_move);
 
-            let expected_error = CommandHandlingError::PlayIsOutOfTurn(Colour::White);
+            let expected_error = MoveError::PlayIsOutOfTurn(Colour::White);
             assert_eq!(result, Err(expected_error));
             assert_ne!(game.get_piece_at_square(&from_square), None);
             assert_eq!(game.get_piece_at_square(&to_square), None);
@@ -157,15 +177,15 @@ mod tests {
 
             let from_square = chess_set::Square::new(Rank::Seven, File::C);
             let to_square = chess_set::Square::new(Rank::Six, File::C);
-            let opening_move = Command::MakeMove {
+            let opening_move = Move::OrdinaryMove {
                 player: Colour::White,
                 from_square,
                 to_square,
             };
 
-            let result = game.handle_command(opening_move);
+            let result = game.make_move(opening_move);
 
-            let expected_error = CommandHandlingError::CannotMoveOpponentPiece(Colour::White);
+            let expected_error = MoveError::CannotMoveOpponentPiece(Colour::White);
             assert_eq!(result, Err(expected_error));
             assert_ne!(game.get_piece_at_square(&from_square), None);
             assert_eq!(game.get_piece_at_square(&to_square), None);
@@ -177,15 +197,15 @@ mod tests {
 
             let from_square = chess_set::Square::new(Rank::Three, File::H);
             let to_square = chess_set::Square::new(Rank::Four, File::H);
-            let opening_move = Command::MakeMove {
+            let opening_move = Move::OrdinaryMove {
                 player: Colour::White,
                 from_square,
                 to_square,
             };
 
-            let result = game.handle_command(opening_move);
+            let result = game.make_move(opening_move);
 
-            let expected_error = CommandHandlingError::ChessboardActionError(
+            let expected_error = MoveError::ChessboardActionError(
                 chess_set::ChessboardActionError::SquareIsEmpty(from_square),
             );
             assert_eq!(result, Err(expected_error));
--- backend/src/domain/gameplay/mod.rs
+++ backend/src/domain/gameplay/mod.rs
@@ -1,6 +1,3 @@
 pub mod chess_set;
-pub mod commands;
 mod game;
 pub mod rulebook;
-
-pub use game::Game;
