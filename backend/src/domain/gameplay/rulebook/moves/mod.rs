c2b86db49dd58adb230dcaa66ea9f744254b698b
edwilson <edward.e.j.wilson@gmail.com>
2024-07-09 08:13:18 +0100

    Rename `base_move` module to `chess_move`

--- backend/src/domain/gameplay/rulebook/moves/mod.rs
+++ backend/src/domain/gameplay/rulebook/moves/mod.rs
@@ -1,9 +1,9 @@
-mod base_move;
 mod castle;
+mod chess_move;
 mod en_passant;
 mod ordinary_move;
 mod translation;
 
-pub use base_move::{Move, MoveValidationError};
+pub use chess_move::{Move, MoveValidationError};
 pub use en_passant::EnPassant;
 pub use ordinary_move::OrdinaryMove;
