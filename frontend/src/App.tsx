5f6bc0de5dc7b2234df429a508fd86bb9b74ed97
edwilson <edward.e.j.wilson@gmail.com>
2024-07-30 08:38:19 +0100

    Move websocket connection out into game component
    - So that it can eventually be moved into the game machine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,14 @@
 import "./styles/App.css";
 import { Game } from "./components/Game.tsx";
 import { GameMachineContext } from "./context.ts";
-import { useGameWebSocket } from "./lib/websocket";
 
 function App() {
-  const { sendMessage, messageHistory, connectionStatus } = useGameWebSocket();
-
   return (
     <>
       <h1>Chess</h1>
-      <div>Connection status: {connectionStatus}</div>
       <GameMachineContext.Provider>
         <Game />
       </GameMachineContext.Provider>
-      <div className="card">
-        <button onClick={() => sendMessage("Hello server")}>
-          Send message
-        </button>
-      </div>
-      <div>
-        {messageHistory
-          .slice()
-          .reverse()
-          .map((message, index) => {
-            return (
-              <>
-                <code key={index}>{message.data}</code>
-                <br />
-              </>
-            );
-          })}
-      </div>
     </>
   );
 }
--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,5 +1,32 @@
-export const Game = () => (
-  <div>
-    <p>I am a chess game.</p>
-  </div>
-);
+import { useGameWebSocket } from "../lib/websocket";
+
+export const Game = () => {
+  const { sendMessage, messageHistory, connectionStatus } = useGameWebSocket();
+  return (
+    <>
+      <div>
+        <p>I am a chess game.</p>
+      </div>
+
+      <div>Connection status: {connectionStatus}</div>
+      <div className="card">
+        <button onClick={() => sendMessage("Hello server")}>
+          Send message
+        </button>
+      </div>
+      <div>
+        {messageHistory
+          .slice()
+          .reverse()
+          .map((message, index) => {
+            return (
+              <>
+                <code key={index}>{message.data}</code>
+                <br />
+              </>
+            );
+          })}
+      </div>
+    </>
+  );
+};
