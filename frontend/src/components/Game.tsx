3d71ed09d88d0181abbe2ad662c6fb6b602b01d1
edwilson <edward.e.j.wilson@gmail.com>
2024-08-02 09:39:33 +0100

    Add font awesome icons to represent the chess pieces

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -2,36 +2,16 @@ import { useSelector } from "@xstate/react";
 
 import { Chessboard } from "./Chessboard";
 import { GameMachineContext } from "../context.ts";
-import { useGameWebSocket } from "../lib/websocket";
+// import { useGameWebSocket } from "../lib/websocket";
 
 export const Game = () => {
-  const { sendMessage, messageHistory, connectionStatus } = useGameWebSocket();
+  // const { sendMessage, messageHistory, connectionStatus } = useGameWebSocket();
   const gameMachineRef = GameMachineContext.useActorRef();
   const game = useSelector(gameMachineRef, (state) => state.context.game);
 
   return (
-    <>
+    <div style={{ width: "1200px", height: "1200px" }}>
       {game && <Chessboard chessboard={game.chessboard} />}
-
-      <div>Connection status: {connectionStatus}</div>
-      <div className="card">
-        <button onClick={() => sendMessage("Hello server")}>
-          Send message
-        </button>
-      </div>
-      <div>
-        {messageHistory
-          .slice()
-          .reverse()
-          .map((message, index) => {
-            return (
-              <>
-                <code key={index}>{message.data}</code>
-                <br />
-              </>
-            );
-          })}
-      </div>
-    </>
+    </div>
   );
 };
