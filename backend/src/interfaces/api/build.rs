26633195cacf6b2561b9d185a4064b2d10487855
edwilson <edward.e.j.wilson@gmail.com>
2024-08-24 09:19:17 +0100

    Take a very liberal approach to cross-origin access

--- backend/src/interfaces/api/build.rs
+++ backend/src/interfaces/api/build.rs
@@ -1,14 +1,27 @@
 use super::routes;
 use rocket;
+use rocket_cors;
 
 pub fn rocket_build() -> rocket::Rocket<rocket::Build> {
-    rocket::build().mount(
-        "/api",
-        rocket::routes![
-            routes::start_game,
-            routes::get_game_state,
-            routes::play_move,
-            routes::play
-        ],
-    )
+    let cors = rocket_cors::CorsOptions::default()
+        .allowed_origins(rocket_cors::AllowedOrigins::all())
+        .allowed_methods(
+            vec![rocket::http::Method::Get, rocket::http::Method::Post]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true);
+
+    rocket::build()
+        .mount(
+            "/api",
+            rocket::routes![
+                routes::start_game,
+                routes::get_game_state,
+                routes::play_move,
+                routes::play
+            ],
+        )
+        .attach(cors.to_cors().unwrap())
 }
