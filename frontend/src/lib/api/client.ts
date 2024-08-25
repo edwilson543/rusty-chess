dacaca4ce6e891b756f3eca39a625475e9a8d491
edwilson <edward.e.j.wilson@gmail.com>
2024-08-25 08:41:26 +0100

    Move logic for handling request response cycle to api lib

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,8 +1,33 @@
 import { initClient } from "@ts-rest/core";
 
-import { contract } from "./contract.ts";
+import { contract, GameSchema } from "./contract.ts";
+import { parseGameSchemaToGame } from "./deserializers.ts";
+import * as types from "../types.ts";
+import { Game } from "../types.ts";
 
-export const APIClient = initClient(contract, {
+export interface APIClient {
+  startGame(): Promise<types.Game>;
+}
+
+export const getApiClient = (): APIClient => {
+  return new RestAPIClient();
+};
+
+const client = initClient(contract, {
   baseUrl: "http://127.0.0.1:8000/api",
   cors: false,
 });
+
+class RestAPIClient implements APIClient {
+  startGame(): Promise<Game> {
+    const promise = client.startGame() as Promise<Response>;
+    return promise.then((response: Response) => {
+      switch (response.status) {
+        case 201:
+          return parseGameSchemaToGame(JSON.parse(response.body) as GameSchema);
+        default:
+          throw new Error(`Error starting game: ${response.status}`);
+      }
+    });
+  }
+}
--- frontend/src/machines/game/actors.ts
+++ frontend/src/machines/game/actors.ts
@@ -1,17 +1,8 @@
 import { fromPromise } from "xstate";
 
-import { APIClient } from "../../lib/api/client.ts";
-import { GameSchema } from "../../lib/api/contract.ts";
-import { parseGameSchemaToGame } from "../../lib/api/deserializers.ts";
+import { getApiClient } from "../../lib/api/client.ts";
 
 export const startGame = fromPromise(() => {
-  const promise = APIClient.startGame() as Promise<Response>;
-  return promise.then((response: Response) => {
-    switch (response.status) {
-      case 201:
-        return parseGameSchemaToGame(JSON.parse(response.body) as GameSchema);
-      default:
-        throw new Error(`Error starting game: ${response.status}`);
-    }
-  });
+  const apiClient = getApiClient();
+  return apiClient.startGame();
 });
