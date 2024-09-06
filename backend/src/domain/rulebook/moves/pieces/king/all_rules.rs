803d1a2c4a5f8ba32b93890ffe2cdab3fe9f7d29
edwilson <edward.e.j.wilson@gmail.com>
2024-09-06 07:22:16 +0100

    Split king rules out int per rule modules

---
+++ backend/src/domain/rulebook/moves/pieces/king/all_rules.rs
@@ -0,0 +1,8 @@
+use super::single_square_any_direction::AllowSingleSquareAnyDirection;
+use crate::domain::rulebook::moves::chess_move;
+use std::vec;
+
+pub fn get_king_move_rules() -> vec::IntoIter<Box<dyn chess_move::MoveRule>> {
+    vec![Box::new(AllowSingleSquareAnyDirection) as Box<dyn chess_move::MoveRule>].into_iter()
+}
+
---
+++ backend/src/domain/rulebook/moves/pieces/king/mod.rs
@@ -0,0 +1,4 @@
+mod all_rules;
+mod single_square_any_direction;
+
+pub use all_rules::get_king_move_rules;
