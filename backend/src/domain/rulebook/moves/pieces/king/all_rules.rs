d7d8b5019a9ba3ab3dca631e92bd0fe9e1e2d811
edwilson <edward.e.j.wilson@gmail.com>
2024-09-06 08:44:16 +0100

    Install castling to king rules

--- backend/src/domain/rulebook/moves/pieces/king/all_rules.rs
+++ backend/src/domain/rulebook/moves/pieces/king/all_rules.rs
@@ -1,8 +1,12 @@
+use super::castle::AllowCastle;
 use super::single_square_any_direction::AllowSingleSquareAnyDirection;
 use crate::domain::rulebook::moves::chess_move;
 use std::vec;
 
 pub fn get_king_move_rules() -> vec::IntoIter<Box<dyn chess_move::MoveRule>> {
-    vec![Box::new(AllowSingleSquareAnyDirection) as Box<dyn chess_move::MoveRule>].into_iter()
+    vec![
+        Box::new(AllowSingleSquareAnyDirection) as Box<dyn chess_move::MoveRule>,
+        Box::new(AllowCastle) as Box<dyn chess_move::MoveRule>,
+    ]
+    .into_iter()
 }
-
