8a31f139b5f7e914eeb48ba15bd670fc5d916fca
edwilson <edward.e.j.wilson@gmail.com>
2024-09-07 17:37:53 +0100

    Add test for get game state endpoint when game does not exist

--- backend/tests/functional/api/test_get_game_state.rs
+++ backend/tests/functional/api/test_get_game_state.rs
@@ -20,3 +20,18 @@ fn can_fetch_state_of_game_that_exists() {
         assert_eq!(response.content_type(), Some(http::ContentType::JSON));
     }
 }
+
+#[test]
+fn not_found_response_when_game_does_not_exist() {
+    {
+        let build = api::rocket_build();
+        let client = local::blocking::Client::tracked(build).unwrap();
+
+        let url = format!("/api/games/{}", 123);
+        let request = client.get(url);
+        let response = request.dispatch();
+
+        assert_eq!(response.status(), http::Status::NotFound);
+        assert_eq!(response.content_type(), Some(http::ContentType::JSON));
+    }
+}
